0c4bd3fa91a3958800d450624257b9ae148fd061
subasish bag <subasishbag77@gmail.com>
2024-11-19 15:59:03 +0530

    new changes to  the scroll

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar/Navbar1';
 import SparklesPreview from './SparklesPreview';
 import './HomePage.css';
@@ -10,18 +10,57 @@ import { Footer } from './pages/footer/Footer';
 import { FAQSection } from './pages/faqsection/FAQSection';
 
 const HomePage = () => {
+  const [activeSection, setActiveSection] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      const scrollPosition = window.scrollY;
+
+      sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollPosition >= sectionTop - sectionHeight / 3) {
+          setActiveSection(index + 1);
+          section.classList.add('scroll-active');
+        } else {
+          section.classList.remove('scroll-active');
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToSection = (index) => {
+    const sections = document.querySelectorAll('section');
+    if (sections[index - 1]) {
+      sections[index - 1].scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="home-page">
       <Navbar />
-     <section> <div className="section1">
-      <SparklesPreview />
+      <div className="side-icons">
+        {[1, 2, 3, 4, 5, 6].map((num) => (
+          <div
+            key={num}
+            className={`side-icon ${activeSection === num ? 'active' : ''}`}
+            onClick={() => scrollToSection(num)}
+          />
+        ))}
+      </div>
+      <section><div className="section1">
+        <SparklesPreview />
       </div></section>
       <section><div className='section2'><Hero /></div></section>
       <section><div className='section3'><CommunityFeatures /></div></section>
-      <section> <div className='section4'><FeaturesSection /></div></section>
+      <section><div className='section4'><FeaturesSection /></div></section>
       <section><div className='section5'><FAQSection /></div></section>
       <section><div className='section6'><Footer /></div></section>
-      
     </div>
   );
 };
